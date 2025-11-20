e5cafe3e93d48ff114852ee6b1cf997b42679de4
uranny <164876325+uranny@users.noreply.github.com>
2025-11-20 10:44:12 +0900

    feat : add base design at main

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,12 @@
 import "./App.css";
 import { BrowserRouter } from "react-router";
 import RoutesSetup from "./routes/RoutesSetup";
-import { useState, useEffect } from "react";
-import { auth } from "./firebase";
-import { onAuthStateChanged, type User } from "firebase/auth";
-import Signin from "./pages/signin";
-import Signup from "./pages/signup";
 
 function App() {
-  const [user, setUser] = useState<User | null>(null);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [signMode, setSignMode] = useState<"in" | "up">("in");
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-      setUser(currentUser);
-      setLoading(false);
-    });
-    return () => unsubscribe();
-  }, []);
-  if (loading) {
-    return <div>로1딩</div>;
-  }
   return (
-    <div className="App">
-      {!user ? (
-        signMode === "in" ? (
-          <div>
-            <a>넌 로1그인이나 해라</a>
-            <Signin />
-            <button onClick={() => setSignMode("up")}>
-              회원가입 하러 가기
-            </button>
-          </div>
-        ) : (
-          <div>
-            <a>넌 회1원가1입이나 해라</a>
-            <Signup />
-            <button onClick={() => setSignMode("in")}>로그인 하러 가기</button>
-          </div>
-        )
-      ) : (
-        <>
-          <BrowserRouter>
-            <RoutesSetup />
-          </BrowserRouter>
-          <button onClick={() => auth.signOut()}>넌 나가라</button>
-        </>
-      )}
-    </div>
+    <BrowserRouter>
+      <RoutesSetup />
+    </BrowserRouter>
   );
 }
 
--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,21 +1,45 @@
-import { useEffect, useState } from "react";
-import getWeather from "../../api/weather";
-import type { WeatherResponse } from "../../types/weather";
+import { useCallback, useEffect, useState } from "react"
+import getWeather from "../../api/weather"
+import type { WeatherResponse } from "../../types/weather"
+import PlantGIF from "../../assets/gif/plant.gif"
+import formatCategory from "../../utils/FormatCategory"
 
 export default function Main() {
-  const [response, setResponse] = useState<WeatherResponse | null>(null);
+    const [response, setResponse] = useState<WeatherResponse | null>(null)
 
-  useEffect(() => {
-    const initWeather = async () => {
-      const data = await getWeather();
-      if (!data) {
-        console.log("값 받아오기 실패");
-        return;
-      }
-      setResponse(data);
-    };
-    initWeather();
-  }, []);
+    useEffect(() => {
+        const initWeather = async () => {
+            const data = await getWeather();
+            if(!data){
+                console.log('값 받아오기 실패')
+                return
+            }
+            setResponse(data)
+        }
+        initWeather()
+    }, [])
 
-  return <div>main</div>;
+    const renderWeatherInfoList = useCallback(() => {
+        return response?.response.body.items.item.map(({category, obsrValue}, i) => {
+            const { label, value } = formatCategory(category, obsrValue);
+            return (
+                <div className="inline-flex flex-row gap-2 p-2 mr-2 mb-2 bg-white flex-wrap" key={i}>
+                    <p>{label}</p>
+                    <p>{value}</p>
+                </div>
+            );
+        });
+    }, [response])
+
+    return (
+        <div className="relative w-screen h-screen bg-blue-200">
+            <div className="gap-3">
+                {renderWeatherInfoList()}
+            </div>
+            <img 
+                className="w-60 left-1/2 top-1/2 -translate-x-1/2 absolute object-contain"
+                src={PlantGIF}
+            />
+        </div>
+    )
 }
