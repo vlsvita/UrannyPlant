1a01a5b64d51d0c7ff48681d5fb9a8bcaa92b72b
vlsvita <vlsvita0@gmail.com>
2025-11-20 14:31:32 +0900

    fix : nx, ny parameter

--- src/api/weather.ts
+++ src/api/weather.ts
@@ -1,35 +1,40 @@
 import type { WeatherResponse } from "../types/weather";
 
-const getWeather = async () : Promise<WeatherResponse | undefined> => {
-    const serviceKey = import.meta.env.VITE_PUBLIC_API_KEY;
-    const pageNo = 3;
-    const numOfRows = 1000;
-    const dataType = "JSON";
+const getWeather = async (
+  userNx: number | undefined,
+  userNy: number | undefined
+): Promise<WeatherResponse | undefined> => {
+  const serviceKey = import.meta.env.VITE_PUBLIC_API_KEY;
+  const pageNo = 3;
+  const numOfRows = 1000;
+  const dataType = "JSON";
 
-    const date = new Date();
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, "0");
-    const day = String(date.getDate()).padStart(2, "0");
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
 
-    const base_date = `${year}${month}${day}`;
-    const base_time = "0600";
+  const base_date = `${year}${month}${day}`;
+  const base_time = "0600";
 
-    const nx = 86;
-    const ny = 86;
+  const nx = userNx;
+  const ny = userNy;
 
-    const url = `${import.meta.env.VITE_PUBLIC_API}?serviceKey=${serviceKey}&pageNo=${pageNo}&numOfRows=${numOfRows}&dataType=${dataType}&base_date=${base_date}&base_time=${base_time}&nx=${nx}&ny=${ny}`;
-    try {
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const data : WeatherResponse = await response.json();
-        console.log(data)
-        return data
-    } catch (error) {
-        console.error("Error fetching weather data:", error);
-        return
+  const url = `${
+    import.meta.env.VITE_PUBLIC_API
+  }?serviceKey=${serviceKey}&pageNo=${pageNo}&numOfRows=${numOfRows}&dataType=${dataType}&base_date=${base_date}&base_time=${base_time}&nx=${nx}&ny=${ny}`;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
-}
+    const data: WeatherResponse = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error("Error fetching weather data:", error);
+    return;
+  }
+};
 
-export default getWeather
+export default getWeather;
--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,45 +1,85 @@
-import { useCallback, useEffect, useState } from "react"
-import getWeather from "../../api/weather"
-import type { WeatherResponse } from "../../types/weather"
-import PlantGIF from "../../assets/gif/plant.gif"
-import formatCategory from "../../utils/FormatCategory"
+import { useCallback, useEffect, useState } from "react";
+import getWeather from "../../api/weather";
+import type { WeatherResponse } from "../../types/weather";
+import PlantGIF from "../../assets/gif/plant.gif";
+import formatCategory from "../../utils/FormatCategory";
+import type { UserData } from "../../types/user";
+import { getUserData } from "../../api/user";
+import { auth } from "../../firebase";
+import { onAuthStateChanged } from "firebase/auth";
 
 export default function Main() {
-    const [response, setResponse] = useState<WeatherResponse | null>(null)
+  const [response, setResponse] = useState<WeatherResponse | null>(null);
+  const [user, setUser] = useState(auth.currentUser);
+  const [userData, setUserData] = useState<UserData | null>();
 
-    useEffect(() => {
-        const initWeather = async () => {
-            const data = await getWeather();
-            if(!data){
-                console.log('값 받아오기 실패')
-                return
-            }
-            setResponse(data)
-        }
-        initWeather()
-    }, [])
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => unsubscribe();
+  }, []);
 
-    const renderWeatherInfoList = useCallback(() => {
-        return response?.response.body.items.item.map(({category, obsrValue}, i) => {
-            const { label, value } = formatCategory(category, obsrValue);
-            return (
-                <div className="inline-flex flex-row gap-2 p-2 mr-2 mb-2 bg-white flex-wrap" key={i}>
-                    <p>{label}</p>
-                    <p>{value}</p>
-                </div>
-            );
-        });
-    }, [response])
+  useEffect(() => {
+    if (!user?.uid) {
+      console.log("사용자가 로그인하지 않았습니다.");
+      return;
+    }
 
-    return (
-        <div className="relative w-screen h-screen bg-blue-200">
-            <div className="gap-3">
-                {renderWeatherInfoList()}
-            </div>
-            <img 
-                className="w-60 left-1/2 top-1/2 -translate-x-1/2 absolute object-contain"
-                src={PlantGIF}
-            />
-        </div>
-    )
+    const fetchUserData = async () => {
+      const data = await getUserData(user.uid);
+      if (!data) {
+        console.log("사용자 정보 받아오기 실패");
+        return;
+      }
+      setUserData(data);
+    };
+
+    fetchUserData();
+  }, [user?.uid]);
+
+  useEffect(() => {
+    if (!userData?.coordinates) {
+      return;
+    }
+    const initWeather = async () => {
+      const data = await getWeather(
+        userData?.coordinates.nx,
+        userData?.coordinates.ny
+      );
+      if (!data) {
+        console.log("값 받아오기 실패");
+        return;
+      }
+      setResponse(data);
+    };
+    initWeather();
+  }, []);
+
+  const renderWeatherInfoList = useCallback(() => {
+    return response?.response.body.items.item.map(
+      ({ category, obsrValue }, i) => {
+        const { label, value } = formatCategory(category, obsrValue);
+        return (
+          <div
+            className="inline-flex flex-row gap-2 p-2 mr-2 mb-2 bg-white flex-wrap"
+            key={i}
+          >
+            <p>{label}</p>
+            <p>{value}</p>
+          </div>
+        );
+      }
+    );
+  }, [response]);
+
+  return (
+    <div className="relative w-screen h-screen bg-blue-200">
+      <div className="gap-3">{renderWeatherInfoList()}</div>
+      <img
+        className="w-60 left-1/2 top-1/2 -translate-x-1/2 absolute object-contain"
+        src={PlantGIF}
+      />
+    </div>
+  );
 }
